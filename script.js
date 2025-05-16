318fb9d777f2754d81c70f1e169fde41b9ad53b9
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-16 09:04:11 +0600

    script js  :bug:

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let searchBtn = document.querySelector('#searchBtn');
 let searchUser = document.querySelector('#searchUser');
 let ui = new UI();
-
+// Script js
 searchBtn.addEventListener('click', (e) => {
     let userText = searchUser.value;
     if (userText != '') {
